7a0044f9ebb0b7ab36abe605079d5fbc3e9fa357
Ezequiel Garriga <ezegarriga.cba@gmail.com>
2022-02-07 21:18:59 -0300

    subida 2do desafio

---
+++ src/components/NavbarComp.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import {Navbar, Nav, NavDropdown, } from 'react-bootstrap'
+
+export default class NavbarComp extends Component {
+    render () {
+        return (
+            <div>
+                <Navbar bg="dark" variant={"dark" }expand="lg">
+                    <Navbar.Brand href="#home">Valhalla Records</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Local</Nav.Link>
+                        <NavDropdown title="Musica" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Vinilos</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Bandejas TocaDiscos</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Auriculares</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Libros Stephen King</NavDropdown.Item>
+                        </NavDropdown>
+                        </Nav>
+                     </Navbar.Collapse>
+                </Navbar>
+            </div>
+        )
+    }
+}
